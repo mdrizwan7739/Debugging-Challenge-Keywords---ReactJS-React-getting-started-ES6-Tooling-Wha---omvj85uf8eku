d1e37b7b8c243442413cae4768081f8c891c0ba5
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-12 14:10:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
 
   return (
     <div className="ball">
-      <h1 classname="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
+      <h1 className="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
       <button className='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
     </div>
   )
